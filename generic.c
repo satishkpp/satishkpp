2246849cc661deb86e4c7873ddc49be8532ee67e
satishkpp <satish.kk@pathpartnertech.com>
2022-02-18 12:11:18 +0530

    generic

---
+++ generic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+   int *ptr_values=NULL;
+   unsigned int value_generator=5;
+   int assigned_value=10;
+   *ptr=&assigned_value;
+   printf("basic destination for code anlaysis is pointer to address:%d",ptr);
+   return 0;
+}
